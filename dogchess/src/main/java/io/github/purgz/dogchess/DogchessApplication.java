34e52632f1194252b7294d2ecd8f6a1bf9263fc0
purgz <henbrooks6@gmail.com>
2023-10-13 12:04:33 +0100

    inital commit

---
+++ dogchess/src/main/java/io/github/purgz/dogchess/DogchessApplication.java
@@ -0,0 +1,13 @@
+package io.github.purgz.dogchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogchessApplication.class, args);
+	}
+
+}
---
+++ dogchess/src/test/java/io/github/purgz/dogchess/DogchessApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.purgz.dogchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
