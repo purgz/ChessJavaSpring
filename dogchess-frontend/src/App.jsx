1c4d6e915e823f50812dc2fa7ae5cea8d8256401
purgz <henbrooks6@gmail.com>
2024-01-13 13:21:20 +0000

    added front end login component

--- dogchess-frontend/src/App.jsx
+++ dogchess-frontend/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import {Outlet, Routes, Route, Navigate} from "react-router-dom";
 import Home from './components/Home';
 import Game from './components/Game';
+import RequireAuth from "./RequireAuth.jsx";
+import Login from "./Login.jsx";
 
 function App() {
 
@@ -12,9 +14,14 @@ function App() {
 
 			<Route path = "/" element = {<Outlet/>}>
 
-				<Route path = "/" element = {<Home/>}></Route>
 
-				<Route path = "/game/:gameId" element = {<Game/>}></Route>
+				<Route path="/" element = {<Login/>}></Route>
+
+				<Route element={<RequireAuth/>}>
+					<Route path = "/home" element = {<Home/>}></Route>
+
+					<Route path = "/game/:gameId" element = {<Game/>}></Route>
+				</Route>
 
 				<Route path = "*" element = {<Navigate to="/" replace></Navigate>}></Route>
 			</Route>
--- dogchess-frontend/src/main.jsx
+++ dogchess-frontend/src/main.jsx
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {AuthProvider} from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <BrowserRouter>
-    
-      <Routes>
+        <AuthProvider>
+          <Routes>
 
-        <Route path = "*" element = {<App></App>}></Route>
+            <Route path = "*" element = {<App></App>}></Route>
 
-      </Routes>
-    
+          </Routes>
+        </AuthProvider>
     </BrowserRouter>
 
   </React.StrictMode>,
